0af9193f99f4715ddb7ddc9c427f7406d4209f74
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2024-04-04 14:36:55 +0100

    Added conditional file loading

--- main.cpp
+++ main.cpp
@@ -4,8 +4,21 @@
 #include <iostream>
 
 int main() {
-    SupplyManagement manager = SupplyManagement("../data/Cities_Madeira.csv", "../data/Reservoirs_Madeira.csv",
-                                                "../data/Stations_Madeira.csv", "../data/Pipes_Madeira.csv");
+    bool useMadeiraCSV = false;
+
+    string cityFile = "../data/Cities.csv";
+    string reservoirFile = "../data/Reservoir.csv";
+    string stationFile = "../data/Stations.csv";
+    string pipeFile = "../data/Pipes.csv";
+    if (useMadeiraCSV) {
+        cityFile = "../data/Cities_Madeira.csv";
+        reservoirFile = "../data/Reservoirs_Madeira.csv";
+        stationFile = "../data/Stations_Madeira.csv";
+        pipeFile = "../data/Pipes_Madeira.csv";
+    }
+
+    SupplyManagement manager = SupplyManagement(cityFile, reservoirFile, stationFile, pipeFile);
+
     manager.createSupers();
     manager.readCities();
     manager.readReservoirs();
--- src/Menu.cpp
+++ src/Menu.cpp
@@ -103,5 +103,3 @@ void Menu::analyseMenu() {
 
     }
 }
-
-
